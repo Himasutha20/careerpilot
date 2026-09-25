d76a0e90c93eb26e7c47a8a4753a0c16b4dbc0f0
Himasutha20 <lingareddyhimasutha@gmail.com>
2026-09-25 11:39:24 +0530

    main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if not GOOGLE_API_KEY:
     raise ValueError("GEMINI_API_KEY environment variable is not set.")
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-3.6-flash",
+    model="gemini-3.5-flash-lite",
     google_api_key=GOOGLE_API_KEY,
     temperature=0
 )
